08f6621f07b831c9ed2b303e65f6ab97f2809a61
Mark Schmale <masch@masch.it>
2012-09-22 16:51:45 +0200

    fade out all popups before opening a new one

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -4,6 +4,7 @@ $('a[href^=#]').click(function(evt) {
   var className = $(this).attr('href').replace(/^#/, 'popup-');
   $ele = $('.' + className);
   if($ele.length) {
+    $('.popup:not(.'+className+')').addClass('hidden-popup');
     $ele.toggleClass('hidden-popup');
     evt.preventDefault();
     return false; 
