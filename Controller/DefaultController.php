3a870e804b8fe487c5b58f0f8446a899f13810ff
Mark Schmale <masch@masch.it>
2012-09-22 16:23:04 +0200

    moved the geshi loading into a new (tiny) bundle

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,19 +9,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
         
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     * @Template()
-     */
-    public function indexAction()
-    {
-      require $this->get('kernel')->getRootDir() . '/../vendor/easybook/geshi/geshi.php';
-
-      $g = new \GeSHi(file_get_contents(__FILE__), 'php');
-      $g->enable_classes();
-      $g->set_header_type(GESHI_HEADER_PRE_TABLE);
-      $g->set_overall_class('code');
-      $g->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
-      return array('code' => $g->parse_code(), 'css' => $g->get_stylesheet());
-    }
+  /**
+   * @Route("/")
+   * @Template()
+   */
+  public function indexAction()
+  {    
+    $g = new \GeSHi(file_get_contents(__FILE__), 'php');
+    $g->enable_classes();
+    $g->set_header_type(GESHI_HEADER_PRE_TABLE);
+    $g->set_overall_class('code');
+    $g->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
+    return array('code' => $g->parse_code(), 'css' => $g->get_stylesheet());
+  }
 }
