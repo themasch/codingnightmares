0f83c42626d2cc9b0643b5aedbb92afe0fe6bb47
Mark Schmale <masch@masch.it>
2012-09-24 16:21:08 +0200

    show errors after failed login, some refactoring

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\SecurityContext;
 use maschit\CodingNightmares\WebsiteBundle\Form\RegistrationForm;
 use maschit\CodingNightmares\WebsiteBundle\Entity\User;
         
@@ -31,12 +32,26 @@ class AuthController extends Controller
 
   /**
    * @Route("/login", name="login")
+   *
+   * this action is only called to show the from. The checks are done inside the 
+   * symfony security layer
+   * 
    * @Template()
    */
   public function loginAction()
   {
-    // TODO
-    return array();
+    $session = $this->request->getSession();
+    $attributes = $this->request->attributes;
+    if($attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+      $error = $attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+    } 
+    else {
+      $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+    }
+    return array(
+      'last_username' => $session->get(SecurityContext::LAST_USERNAME), 
+      'error' => $error
+    );
 
   }
 
@@ -51,6 +66,7 @@ class AuthController extends Controller
       $form->bind($this->request);
       if(!$form->isValid())
         return array('reg_form' => $form->createView());
+      //@TODO: check if users exists
       $data = $form->getData();
       $user = new User();
       $encoder = $this->encoderFactory->getEncoder($user);
@@ -59,10 +75,10 @@ class AuthController extends Controller
       $user->setMail($data['email']);
       $this->em->persist($user);
       $this->em->flush();
-      return new Response('welcome!'); 
+      // @TODO send user an verifycation email
+      return new Response('welcome!');  //@TODO: create view
     }
     return array('reg_form' => $form->createView());
-    //$user->setMail($data['email'])->setPassword();
 
   }
 }
--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -1,12 +1,24 @@
+/*
+$('.popup-login form').submit(function(evt) {
+  var data = $(this).serialize();
+  var path = $(this).attr('action');
+  $.post(path, data, function(d) {
+    console.log(d);
+  });
+  evt.preventDefault();
+  return false;
+});
+ */
 
 // open popups with links
-$('a[href^=#]').click(function(evt) {
-  var className = $(this).attr('href').replace(/^#/, 'popup-');
+$('a[class^=btn-]').click(function(evt) {
+  var className = $(this).attr('class').replace(/^btn-/, 'popup-');
   $ele = $('.' + className);
   if($ele.length) {
     $('.popup:not(.'+className+')').addClass('hidden-popup');
     $ele.toggleClass('hidden-popup');
+    $ele.find('input:first').focus();
     evt.preventDefault();
-    return false; 
+    return false;
   }
 });
