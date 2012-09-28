d26fabb908ce5af146907f46c03c3e0ed26ccc92
Mark Schmale <masch@masch.it>
2012-09-28 17:24:37 +0200

    added first menu points & prepared template for real data

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,7 +7,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use maschit\CodingNightmares\WebsiteBundle\Form\RegistrationForm;
 
-        
+use maschit\CodingNightmares\WebsiteBundle\Entity\Post;
+use maschit\CodingNightmares\WebsiteBundle\Entity\Language;
+
 class DefaultController extends Controller
 {
   /**
@@ -16,16 +18,26 @@ class DefaultController extends Controller
    */
   public function indexAction()
   {    
+
+    // @TODO: posts from database
     $g = new \GeSHi(file_get_contents(__FILE__), 'php');
     $g->enable_classes();
     $g->set_header_type(GESHI_HEADER_PRE_TABLE);
     $g->set_overall_class('code');
     $g->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
-
-    $regform = $this->createForm(new RegistrationForm());
+    // @TODO: geshi as a twig extension (filter)
+    // post.content|geshi(code.lang.name)
+    $lang = new Language();
+    $lang->setName('php');
+    $post = new Post();
+    $post->setTitle("bad symfony2 controller");
+    $post->setLang($lang);
+    $post->setDateAdded(new \DateTime());
+    $post->setContent(file_get_contents(__FILE__));
     return array(
+      'posts' => [$post, $post], 
       'code' => $g->parse_code(), 
-      'reg_form' => $regform->createView(),
+      'reg_form' => $this->createForm(new RegistrationForm())->createView(),
       'includeForms' => 'true');
   }
 }
